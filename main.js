2c72b8444d599164b4e649bdd51d2f05873fd09c
YaruTK <mcdonalddark@gmail.com>
2025-01-09 14:33:16 +0100

    ThisIsTheStart

---
+++ main.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.loop = void 0;
+const loop = () => {
+    console.log("Hello Screeps!");
+};
+exports.loop = loop;
